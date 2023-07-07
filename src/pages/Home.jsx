babb4cbc4aa39dee8edc95ffc7306ebbdf25e892
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-07-06 23:18:55 -0400

    assets added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import Wrapper from '../components/ui/wrapper/Wrapper'
+import img1 from "../assets/IMG_2120.jpg"
+import img2 from "../assets/IMG_2121.jpg"
 function Home() {
   return (
 
@@ -8,7 +10,8 @@ function Home() {
       <h3>I am an artist that loves to Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas optio rerum beatae consectetur veniam assumenda consequuntur eveniet temporibus earum iste.</h3>
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates ut laboriosam nihil fugit voluptatibus amet maxime aspernatur animi eos? Praesentium hic non minima necessitatibus repellendus quod accusamus tenetur ut! Accusamus, nostrum molestiae cum adipisci similique doloremque nulla assumenda ex expedita, hic ipsa debitis quod recusandae modi velit quisquam possimus!</p>
-
+      <img src={img1} alt="aaron-one" style={{maxWidth: "20rem"}} />
+      <img src={img2} alt="aaron-two" style={{maxWidth: "20rem", maxHeight: "20rem"}} />
     </Wrapper>
   )
 }
